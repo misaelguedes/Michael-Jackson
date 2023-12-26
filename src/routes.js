4665db51c85f9f334792655b8d62f46e0c9a8b82
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-12-26 13:19:48 -0300

    Criando Footer

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
 
 export default function RoutesApp() {
     return (
         <BrowserRouter>
             <Header/>
+            <Footer/>
         </BrowserRouter>
     )
 }
